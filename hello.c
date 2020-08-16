d2603e3f32d0dcf4466d50bf943fe8b77a2e4f3e
Carlos Diaz <cdiaz6@husker.unl.edu>
2020-08-16 17:45:40 +0000

    changed message to my name

--- hello.c
+++ hello.c
@@ -1,16 +1,16 @@
 /**
  * Author: Carlos Diaz
  * Date: 2020
- * 
+ *
  * A simple hello world program
  */
  #include <stdlib.h>
  #include <stdio.h>
- 
+
  int main(int argc, char **argv) {
-     
-     printf("Hello World!\n");
-     
+
+     printf("Carlos Diaz!\n");
+
      return 0;
- 
+
 }
